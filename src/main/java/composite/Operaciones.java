b682ef76bf1b76c889fad2e16c5d72f0c42f7d7f
nataliaAznar <natalia.aznar@opendeusto.es>
2014-10-11 11:53:12 +0200

    composite.expresion: atributos protected

--- src/main/java/composite/Operaciones.java
+++ src/main/java/composite/Operaciones.java
@@ -1,8 +1,8 @@
 package composite;
 
 public abstract class Operaciones extends Expresion{
-	Expresion exp1;
-	Expresion exp2;
+	protected Expresion exp1;
+	protected Expresion exp2;
 	
 	public Operaciones (Expresion exp1, Expresion exp2){
 		this.exp1 = exp1;
