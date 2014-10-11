2314128fafbc81ce3e9930168dddcba841c22ac1
nataliaAznar <natalia.aznar@opendeusto.es>
2014-10-11 10:29:12 +0200

    singleton.factory

---
+++ src/main/java/singleton/FactoryReferences.java
@@ -0,0 +1,37 @@
+package singleton;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FactoryReferences {
+	private static FactoryReferences f; 
+    private Map<String, Integer> references;
+
+    private int reference;
+
+    private FactoryReferences() {
+        this.references = new HashMap<>();
+        this.reference = 0;
+    }
+    
+    public static FactoryReferences getFactory(){
+    	if( f == null)
+    		f = new FactoryReferences();
+    	return f;
+    }
+
+    public int getReference(String key) {
+        Integer result = this.references.get(key);
+        if (result == null) {
+            this.references.put(key, this.reference);
+            result = this.reference;
+            reference++;
+        }
+        return result;
+    }
+
+    public void removeReference(String key) {
+        this.references.remove(key);
+    }
+
+}
