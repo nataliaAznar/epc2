a2cf5464f5d9634bb60ea59d66e5e8ea6f50e831
nataliaAznar <natalia.aznar@opendeusto.es>
2014-10-18 09:20:32 +0200

    FactoruMethod

--- src/main/java/factoryMethod/NaturalNumber.java
+++ src/main/java/factoryMethod/NaturalNumber.java
@@ -2,30 +2,30 @@ package factoryMethod;
 
 public class NaturalNumber {
 	protected int value;
-	
+
 	public NaturalNumber(int value) {
-        this.setValue(value);
-    }
-	
+		this.setValue(value);
+	}
+
 	public void setValue(int value) {
-        assert value >= 0;
-        this.value = value;
-    }
-	
+		assert value >= 0;
+		this.value = value;
+	}
+
 	public int getValue() {
-        return value;
-    }
-	
-	 public void add(int value) {
-	        this.setValue(this.value + value);
-	    }
-	 
-	 public String getTextValue(String[] textValue) {
-	        if (this.value < textValue.length) {
-	            return textValue[this.value];
-	        } else {
-	            return "???";
-	        }
-	    }
+		return value;
+	}
+
+	public void add(int value) {
+		this.setValue(this.value + value);
+	}
+
+	public String getTextValue(String[] textValue) {
+		if (this.value < textValue.length) {
+			return textValue[this.value];
+		} else {
+			return "???";
+		}
+	}
 
 }
